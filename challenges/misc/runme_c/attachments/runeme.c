9a905fa3d726254a3f14e34a954d100fe1bdbd3f
Yeol <145646018+SVUCTF@users.noreply.github.com>
2023-10-09 06:42:02 +0800

    添加 misc/runme_c

---
+++ challenges/misc/runme_c/attachments/runeme.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    unsigned char enc_data[21] = "fmcd\x7FMGOIVIT=~QJ$bk2i";
+    unsigned char flag[21];
+    for(int i = 0; i < 21; i++)
+    {
+        flag[i]=enc_data[i]^i;
+    }
+    printf("%s\n", flag);
+    return 0;
+}
