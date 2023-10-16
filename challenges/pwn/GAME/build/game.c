6baaa062b2bab4939ce9de85abfa2c9d9fcf1b8a
Yeol <136407746+13m0n4de@users.noreply.github.com>
2023-10-16 21:46:11 +0800

    尝试修复缓冲区问题

--- challenges/pwn/GAME/build/game.c
+++ challenges/pwn/GAME/build/game.c
@@ -1,71 +1,72 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
 
-enum GAME
-{
-    EXIT,
-    PLAY,
+enum GAME {
+  EXIT,
+  PLAY,
 };
 
-void init()
-    {
-        setvbuf(stdout,0,2,0);
-        setvbuf(stdin,0,2,0);
-        setvbuf(stderr,0,2,0);
-    }
+void init() {
+  setvbuf(stdout, 0, 2, 0);
+  setvbuf(stdin, 0, 2, 0);
+  setvbuf(stderr, 0, 2, 0);
+}
 
 void menu() {
 
-    puts("********************************");
-    puts("+****** 猜数字游戏(1~100) *****+");
-    puts("|*********** 0.EXIT ***********|");
-    puts("+*********** 1.PLAY ***********+");
-    puts("********************************");
+  puts("********************************\n"
+       "+****** 猜数字游戏(1~100) *****+\n"
+       "|*********** 0.EXIT ***********|\n"
+       "+*********** 1.PLAY ***********+\n"
+       "********************************\n");
 }
 
 void game() {
 
-    int sj = rand() % 999 + 1;
-    int cs = 0;
-    int n = 0;
+  int sj = rand() % 999 + 1;
+  int cs = 0;
+  int n = 0;
 
-    while(++n < 3) {
+  while (++n < 3) {
 
-        puts("Please Input:");
-        scanf("%d", &cs);
-        if (sj > cs)
-            puts("It's too small!");
-        else if (sj < cs)
-            puts("It's too big!");
-        else {
-            puts("YOU ARE TRUE!");
-            system("/bin/sh");
-            break;
-        }
+    puts("Please Input:");
+    scanf("%d", &cs);
+    if (sj > cs)
+      puts("It's too small!");
+    else if (sj < cs)
+      puts("It's too big!");
+    else {
+      puts("YOU ARE TRUE!");
+      system("/bin/sh");
+      break;
     }
+  }
 }
 
 int main() {
 
-    int input;
+  int input;
 
-    init();
-    srand(time(0));
-    do {
+  init();
+  srand(time(0));
+  do {
 
-        menu();
-        scanf("%d",&input);
-        switch(input) {
-            case PLAY:
-                game(); break;
-            case EXIT:
-                puts("Exit!"); break;
-            default:
-                puts("Inputs Error, Please Try Again!"); break;
-        }
+    menu();
+    scanf("%d", &input);
+    switch (input) {
+        case PLAY:
+          game();
+          break;
+        case EXIT:
+          puts("Exit!");
+          break;
+        default:
+          puts("Inputs Error, Please Try Again!");
+          break;
+    }
 
-    } while(input);
+  } while (input);
 
-    return 0;
+  return 0;
 }
